c361d2ba3e4a1e34a30e3be59b2397ebd8c88ca5
Emily Wade <emilywade314@gmail.com>
2025-04-06 10:49:24 +0100

    check table availability before booking

--- bookings/views.py
+++ bookings/views.py
@@ -5,7 +5,9 @@ from .forms import BookingForm
 
 
 def get_available_tables(date, time, num_guests):
+    
     booking_start = time
+    time = datetime.strptime(time, "%H:%M").time()
     booking_end = (datetime.combine(date, time) + timedelta(hours=2)).time()
 
     candidate_tables = Table.objects.filter(capacity__gte=num_guests)
@@ -26,10 +28,15 @@ def create_booking(request):
     if request.method == 'POST':
         form = BookingForm(request.POST)
         if form.is_valid():
-            data = form.cleaned_data
-            print("booking data:", data)
-        else:
-            print("form is invalid:", form.errors)  # <-- This will help you debug
+            date = form.cleaned_data['date']
+            time = form.cleaned_data['time']
+            num_guests = form.cleaned_data['num_guests']
+            
+            available_tables = get_available_tables(date, time, num_guests)
+            
+            
+            print("Available tables:", available_tables)
+            
     else:
         form = BookingForm()
     return render(request, 'bookings/create_booking.html', {'form': form})
