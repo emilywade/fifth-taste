f2a8d2ba39f9c272280a0d59770014509fdae5d1
Emily Wade <emilywade314@gmail.com>
2025-04-06 11:21:40 +0100

    make admin panel more user friendly

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Table, Booking
 
+
+class BookingAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'email', 'date', 'time']
+
+    list_filter = ['date', 'time', 'num_guests']
+
+    list_display = ['name', 'email', 'date', 'time', 'num_guests', 'special_requests']
+
+    list_editable = ['num_guests', 'special_requests']
+
+    fields = ['name', 'email', 'date', 'time', 'num_guests', 'special_requests']
+
+
 # Register your models here.
 admin.site.register(Table)
-admin.site.register(Booking)
+admin.site.register(Booking, BookingAdmin)
